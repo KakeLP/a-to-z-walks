53cc24e11290f3226b55d57f5e26287f7c3b420c
Kake <kake@earth.li>
2017-02-22 16:07:49 +0000

    Remove no-longer-working MapQuest tiles.

--- site/js/display.js
+++ site/js/display.js
@@ -3,15 +3,13 @@ $( document ).ready( function() {
   // Set up the map.
   var m = L.map( 'map' ).setView( [51.496775, -0.11652375], 10 );
 
-  var mapQuestAttr = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; ';
   var osmDataAttr = 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-  var mopt = {
-    url: 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg',
-    options: { attribution: mapQuestAttr + osmDataAttr, subdomains: '1234' }
-  };
-  var mq = L.tileLayer( mopt.url, mopt.options );
-  mq.addTo( m );
+  var osm_layer = new L.TileLayer(
+                          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          { attribution: osmDataAttr } );
+
+  osm_layer.addTo( m );
 
   // Add the GeoJSON data layers.
   function popUp( f, l ) {
